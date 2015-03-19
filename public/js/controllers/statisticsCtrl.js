d6f3143b1aae203d146ea4afff2cfb39bde0d095
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2015-03-19 10:16:28 +0300

    updating statistical information

--- public/js/controllers/statisticsCtrl.js
+++ public/js/controllers/statisticsCtrl.js
@@ -140,6 +140,7 @@ angular.module('malariaApp').controller('statisticsCtrl',function($scope,$http){
     };
 
     $scope.prepareSeries = function(){
+
         $scope.changeCats();
         $scope.normalseries = [];
            if($scope.data.chartType == "pie"){
@@ -214,7 +215,7 @@ angular.module('malariaApp').controller('statisticsCtrl',function($scope,$http){
             text: 'Combination chart'
         },
         xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Bananas', 'Plums']
+            categories: []
         },
         labels: {
             items: [{
@@ -226,52 +227,8 @@ angular.module('malariaApp').controller('statisticsCtrl',function($scope,$http){
                 }
             }]
         },
-        series: [{
-            type: 'column',
-            name: 'Jane',
-            data: [3, 2, 1, 3, 4]
-        }, {
-            type: 'column',
-            name: 'John',
-            data: [2, 3, 5, 7, 6]
-        }, {
-            type: 'column',
-            name: 'Joe',
-            data: [4, 3, 3, 9, 0]
-        }, {
-            type: 'spline',
-            name: 'Average',
-            data: [3, 2.67, 3, 6.33, 3.33],
-            marker: {
-                lineWidth: 2,
-                lineColor: Highcharts.getOptions().colors[3],
-                fillColor: 'white'
-            }
-        }, {
-            type: 'pie',
-            name: 'Total consumption',
-            data: [{
-                name: 'Jane',
-                y: 13,
-                color: Highcharts.getOptions().colors[0] // Jane's color
-            }, {
-                name: 'John',
-                y: 23,
-                color: Highcharts.getOptions().colors[1] // John's color
-            }, {
-                name: 'Joe',
-                y: 19,
-                color: Highcharts.getOptions().colors[2] // Joe's color
-            }],
-            center: [100, 80],
-            size: 100,
-            showInLegend: false,
-            dataLabels: {
-                enabled: false
-            }
-        }]
+        series: []
     };
-
 //    $scope.changeCats();
 
 })
